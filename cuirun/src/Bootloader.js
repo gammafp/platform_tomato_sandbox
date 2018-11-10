0fb5bffbd8c596e710f0c762196c4b052cdca9db
gammafp <gammafp@gmail.com>
2018-11-10 11:10:24 +0100

    Fix jump

--- cuirun/src/Bootloader.js
+++ cuirun/src/Bootloader.js
@@ -6,8 +6,8 @@ class Bootloader extends Phaser.Scene {
     preload() {
         this.load.path = './assets/';
 
-        this.load.animation('cobayaData', 'cobaya/guinea_anim.json');
-        this.load.atlas('cobaya', 'cobaya/guinea.png', 'cobaya/guinea_atlas.json');
+        this.load.animation('cobayaData', 'cobaya/cobaya_anim.json');
+        this.load.atlas('cobaya', 'cobaya/cobaya.png', 'cobaya/cobaya_atlas.json');
 
         this.load.animation('monedasData', 'objetos/monedas/monedas_anim.json');
         this.load.atlas('monedas', 'objetos/monedas/monedas.png', 'objetos/monedas/monedas_atlas.json');
